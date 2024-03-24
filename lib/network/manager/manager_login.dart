2813b170d6ed8eab8ce8837eee02ada8e8df408a
Neloy-SWE <taufiqneloy.swe@gmail.com>
2024-03-25 02:28:35 +0600

    login manager added

---
+++ lib/network/manager/manager_login.dart
@@ -0,0 +1,8 @@
+/// Created by Neloy on 3/25/2024
+/// Email: taufiqneloy.swe@gmail.com
+
+abstract class LoginManager {
+  void success({required String message});
+
+  void fail({required String message});
+}
