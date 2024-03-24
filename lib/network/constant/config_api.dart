b910dfdd91ab62d32aef5034b4504afc86647e3b
Neloy-SWE <taufiqneloy.swe@gmail.com>
2024-03-25 02:29:01 +0600

    configs added

---
+++ lib/network/constant/config_api.dart
@@ -0,0 +1,8 @@
+/// Created by Neloy on 3/25/2024
+/// Email: taufiqneloy.swe@gmail.com
+
+class APIConfig {
+  static const String baseUrl = "https://earlygoldsled35.conveyor.cloud/";
+  static const String prefix = "apiAdmin/youBike/";
+  static const String login = "${prefix}Login";
+}
---
+++ lib/network/constant/config_values.dart
@@ -0,0 +1,11 @@
+/// Created by Neloy on 3/25/2024
+/// Email: taufiqneloy.swe@gmail.com
+
+class ValueConfigs {
+  static const String requestPost = "POST";
+
+  static const String contentType = "Content-Type";
+  static const String applicationJson = "application/json";
+  static const String success = "success";
+  static const String error = "error";
+}
