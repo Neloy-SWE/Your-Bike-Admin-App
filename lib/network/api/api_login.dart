da1e89a766d09408e6e63d4be34b520383f93a2f
Neloy-SWE <taufiqneloy.swe@gmail.com>
2024-03-25 02:29:28 +0600

    api call for login class added

---
+++ lib/network/api/api_login.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:your_bike_admin/network/constant/config_api.dart';
+import 'package:your_bike_admin/network/constant/config_values.dart';
+
+/// Created by Neloy on 3/24/2024
+/// Email: taufiqneloy.swe@gmail.com
+
+class LoginAPI {
+  Future<String> run({
+    required String phone,
+    required password,
+  }) async {
+    try {
+      var request = http.Request(
+        ValueConfigs.requestPost,
+        Uri.parse(
+          APIConfig.baseUrl + APIConfig.login,
+        ),
+      );
+
+      var headers = {
+        ValueConfigs.contentType: ValueConfigs.applicationJson,
+      };
+
+      request.body = json.encode({
+        "id": "1",
+        "phone": phone,
+        "password": password,
+      });
+      request.headers.addAll(headers);
+      http.StreamedResponse response = await request.send();
+
+      return await response.stream.bytesToString();
+    } catch (e) {
+      return ValueConfigs.error;
+    }
+  }
+}
