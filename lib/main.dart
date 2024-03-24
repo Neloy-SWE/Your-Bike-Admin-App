79a64eec0c205213c124feaed44528030c4ed5f9
Neloy-SWE <taufiqneloy.swe@gmail.com>
2024-03-25 02:30:00 +0600

    hive configuration added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:your_bike_admin/components/theme/app_theme.dart';
 import 'package:your_bike_admin/screens/screen_splash.dart';
 import 'package:your_bike_admin/utilities/app_strings.dart';
@@ -6,9 +9,14 @@ import 'package:your_bike_admin/utilities/app_strings.dart';
 /// Created by Neloy on 3/16/2024
 /// Email: taufiqneloy.swe@gmail.com
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDirectory = await getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
   runApp(
-    const MyApp(),
+    const ProviderScope(
+      child: MyApp(),
+    ),
   );
 }
 
