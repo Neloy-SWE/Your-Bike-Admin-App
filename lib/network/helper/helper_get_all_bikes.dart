dc70be5161d0ceb8338935f525e21aff205d01de
Neloy-SWE <taufiqneloy.swe@gmail.com>
2024-03-26 14:15:15 +0600

    helper added for get all bike

---
+++ lib/network/helper/helper_get_all_bikes.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:your_bike_admin/network/api/api_get_all_bikes.dart';
+import 'package:your_bike_admin/network/manager/manager_get_all_bikes.dart';
+
+import '../../utilities/app_strings.dart';
+import '../constant/config_values.dart';
+import '../model/model_base.dart';
+import '../model/model_bike.dart';
+
+/// Created by Neloy on 3/25/2024
+/// Email: taufiqneloy.swe@gmail.com
+
+class GetAllBikesHelper implements GetAllBikesManager {
+  Future<void> connection({
+    required GetAllBikesManager manager,
+  }) async {
+    String result = await GetAllBikesAPI().run();
+
+    if (result != ValueConfigs.error) {
+      BaseModel baseModel = BaseModel.fromJson(result);
+      if (baseModel.status == ValueConfigs.success) {
+        List<BikeModel> bikes =
+        baseModel.data.map<BikeModel>((e) => BikeModel.fromMap(e)).toList();
+        manager.success(bikes: bikes, message: baseModel.message!);
+      } else {
+        manager.fail(
+          message: baseModel.message!,
+        );
+      }
+    } else {
+      manager.fail(
+        message: AppStrings.checkYourInternet,
+      );
+    }
+  }
+
+  @override
+  void fail({required String message}) {}
+
+  @override
+  void success({required List<BikeModel> bikes, required String message}) {}
+}
