2a44920fd26f7871ffee7617e4631f1e34bb33a5
Neloy-SWE <taufiqneloy.swe@gmail.com>
2024-03-27 18:33:09 +0600

    add bike api added

---
+++ lib/network/api/api_add_bike.dart
@@ -0,0 +1,39 @@
+import 'package:hive/hive.dart';
+import 'package:http/http.dart' as http;
+import 'package:your_bike_admin/network/model/model_add_bike.dart';
+
+import '../constant/config_api.dart';
+import '../constant/config_values.dart';
+
+/// Created by Neloy on 3/26/2024
+/// Email: taufiqneloy.swe@gmail.com
+
+class AddBikeAPI {
+  Future<String> run({required AddBikeModel bike}) async {
+    try {
+      var box = await Hive.openBox(ValueConfigs.token);
+
+      var request = http.Request(
+        ValueConfigs.requestPost,
+        Uri.parse(
+          APIConfig.baseUrl + APIConfig.addBike,
+        ),
+      );
+
+      request.body = bike.toJson();
+
+      var headers = {
+        ValueConfigs.authorization:
+            ValueConfigs.bearer + await box.get(ValueConfigs.token),
+        ValueConfigs.contentType: ValueConfigs.applicationJson,
+      };
+
+      request.headers.addAll(headers);
+      http.StreamedResponse response = await request.send();
+
+      return await response.stream.bytesToString();
+    } catch (e) {
+      return ValueConfigs.error;
+    }
+  }
+}
