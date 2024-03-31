aa06a74eabc94d14974e0c59ce6871a2e6378755
Neloy-SWE <taufiqneloy.swe@gmail.com>
2024-03-31 23:22:58 +0600

    delete manager added

---
+++ lib/network/manager/manager_delete_bike.dart
@@ -0,0 +1,8 @@
+/// Created by Neloy on 3/31/2024
+/// Email: taufiqneloy.swe@gmail.com
+
+abstract class BikeDetailsManager {
+  void success({required String message});
+
+  void fail({required String message});
+}
