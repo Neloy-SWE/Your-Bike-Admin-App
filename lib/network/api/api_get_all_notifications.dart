a20dd451476cd060bfc2c2143d08eb83ce321962
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2025-01-18 20:33:36 +0600

    get all notification api added

---
+++ lib/network/api/api_get_all_notifications.dart
@@ -0,0 +1,37 @@
+import 'package:http/http.dart' as http;
+import 'package:hive/hive.dart';
+import '../constant/config_api.dart';
+import '../constant/config_values.dart';
+
+/*
+* Created by Neloy on 20 December, 2024.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+class GetAllNotificationAPI {
+  Future<String> run() async {
+    try {
+      var box = await Hive.openBox(ValueConfigs.token);
+
+      var request = http.Request(
+        ValueConfigs.requestGet,
+        Uri.parse(
+          APIConfig.baseUrl + APIConfig.getAllNotifications,
+        ),
+      );
+
+      var headers = {
+        ValueConfigs.authorization:
+            ValueConfigs.bearer + await box.get(ValueConfigs.token),
+        ValueConfigs.accept: ValueConfigs.applicationJson,
+      };
+
+      request.headers.addAll(headers);
+      http.StreamedResponse response = await request.send();
+
+      return await response.stream.bytesToString();
+    } on Exception {
+      return ValueConfigs.error;
+    }
+  }
+}
